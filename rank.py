f96e327ae1bb8e0e54a7949e23498dee50b88ea2
iarmourgarb <74074190+iarmourgarb@users.noreply.github.com>
2021-01-23 17:10:39 -0500

    Update rank.py

--- rank.py
+++ rank.py
@@ -4,9 +4,9 @@ Ranks how "desirable" a place is based on type, price level, rating, and distanc
 
 def rank(places,dist):
     type_list = ['amusement_park', 'aquarium', 'art_gallery', 'bakery', 'bicycle_store', 'book_store', 'bowling_alley', 'cafe', 'clothing_store', 'library', 'movie_theater', 'museum', 'park', 'shopping_mall', 'stadium', 'tourist_attraction', 'zoo', 'food', 'landmark', 'natural_feature', 'point_of_interest', 'town_square']
-    for (place_id,info_dict) in places:
+    for place_id in places:
         rank = 0
-        keys = info_dict.keys()
+        keys = places[place_id].keys()
         if ('types' in keys): #check if type is in type_list. if it isn't, rank += 5
             if (places[place_id]['types'] not in type_list):
                 rank += 5
