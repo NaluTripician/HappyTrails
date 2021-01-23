9615043a248028c20c5b1855d2003b39fd1f0de7
iarmourgarb <74074190+iarmourgarb@users.noreply.github.com>
2021-01-23 15:35:19 -0500

    Update rank.py

--- rank.py
+++ rank.py
@@ -2,15 +2,17 @@
 Ranks how "desirable" a place is based on type, price level, rating, and distance from the original route
 """
 
-def rank(place_dict)
-    types = [amusement_park, aquarium, art_gallery, bakery, bicycle_store, book_store, bowling_alley, cafe, clothing_store, library, movie_theater, museum, park, shopping_mall, stadium, tourist_attraction, zoo, food, landmark, natural_feature, point_of_interest, town_square]
-    for (key,val) in place_dict
-        (key,val) = (place_id, (lat, lng, rating, placeID, type, price_level, website))
+def rank(places)
+    type_list = [amusement_park, aquarium, art_gallery, bakery, bicycle_store, book_store, bowling_alley, cafe, clothing_store, library, movie_theater, museum, park, shopping_mall, stadium, tourist_attraction, zoo, food, landmark, natural_feature, point_of_interest, town_square]
+    for (place_id,info_dict) in places:
+        rank = 0
+        keys = info_dict.keys()
+        if ('types' in keys): #check if type is in type_list. if it isn't, rank += 5
+            if (places[place_id]['types'] not in type_list):
+                rank += 5
+        if ('price_level' in keys):
+            rank += 2*(places[place_id]['price_level'])
+        if ('rating' in keys):
+            rank += 5*(1//(places[place_id]['rating']))
 #        distance = jack's code
-        if type not in types:
-            type_val = 5
-        else:
-            type_val = 0
-        rank = (2*price_level) + (5*(1//rating)) + (4*distance) + type_val
-        (key,val) = (key, rank)
     return({key: val for key, val in sorted(place_dict.items(), key=lambda item: item[1])})
