9759c8dec544e50a14509621c74a56d6725bc78c
Daniel Knopf <danielkn609@gmail.com>
2021-01-23 22:52:09 -0500

    changed the urls so they no longer require quire stuff

--- HappyTrailsApp/urls.py
+++ HappyTrailsApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('<int:question_id>/', views.nalu, name='nalu'),
     path('locations/', views.locations, name='locations'),
 ]
--- HappyTrailsSite/urls.py
+++ HappyTrailsSite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('HappyTrailsApp/', include('HappyTrailsApp.urls')),
+    path('', include('HappyTrailsApp.urls')),
     path('admin/', admin.site.urls),
 ]
