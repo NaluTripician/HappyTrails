12df587e77862c18cd7ab9f1e25ba050906bb734
Nalu Tripician <nalutrip@icloud.com>
2021-01-23 23:24:36 -0500

    index error fixed

--- requestRoute.py
+++ requestRoute.py
@@ -55,10 +55,10 @@ def route(start,end,apiKey,mode='walking'):
     else:
         #idx = random.sample(list(range(0,len(path)-2)),k=5)
         idx = np.round(np.linspace(0, len(path) - 1, 6)).astype(int)
-        print(idx)
+
         POI = []
         for i in idx:
-            if(i<len(idx)-1):
+            if(i<len(path)-1):
                 POI.append( ( ((path[i][0] + path[i+1][0])/2), ((path[i][1] + path[i+1][1])/2)))
 
     # if(len(path)<5):
