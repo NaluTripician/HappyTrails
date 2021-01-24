3841b04483308c68fd5e9466b450fabbe220098c
Nalu Tripician <nalutrip@icloud.com>
2021-01-24 10:43:09 -0500

    removed comments

--- requestRoute.py
+++ requestRoute.py
@@ -60,13 +60,7 @@ def route(start,end,apiKey,mode='walking'):
         for i in idx:
             if(i<len(path)-1):
                 POI.append( ( ((path[i][0] + path[i+1][0])/2), ((path[i][1] + path[i+1][1])/2)))
-    # if(len(path)<5):
-    #     idx = np.round(np.linspace(0, len(path) - 1, len(path))).astype(int)
-    # else:
-    #     idx = np.round(np.linspace(0, len(path) - 1, 5)).astype(int)
-    #
-    # POI = path[idx]
-    #
+
     if(dist<10000):
         radius="500"
     elif(dist<60000):
