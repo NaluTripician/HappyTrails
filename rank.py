acf51c91caaff22b46472373473fe79c66d1124c
iarmourgarb <74074190+iarmourgarb@users.noreply.github.com>
2021-01-24 00:34:29 -0500

    Update rank.py

--- rank.py
+++ rank.py
@@ -3,7 +3,7 @@ import random
 Ranks how "desirable" a place is based on type, price level, rating, and distance from the original route
 """
 def rank2(places, dist):
-    type_list = ['amusement_park', 'aquarium', 'art_gallery', 'bakery', 'bicycle_store', 'book_store', 'bowling_alley', 'cafe', 'clothing_store', 'library', 'movie_theater', 'museum', 'park', 'shopping_mall', 'stadium', 'tourist_attraction', 'zoo', 'food', 'landmark', 'natural_feature', 'town_square']
+    type_list = ['amusement_park', 'aquarium', 'art_gallery', 'bakery', 'bicycle_store', 'book_store', 'bowling_alley', 'cafe', 'library', 'museum', 'park', 'shopping_mall', 'stadium', 'tourist_attraction', 'zoo', 'landmark', 'natural_feature', 'town_square']
     bad_ids = []
 
     for place_id in places:
@@ -22,20 +22,21 @@ def rank2(places, dist):
             if ('price_level' in keys):
                 rank += 2*(places[place_id]['price_level'])
             if ('rating' in keys):
-                rank += 10*(1//(places[place_id]['rating']))
-            rank+= 5*(places[place_id]['distance']/dist)
+                rank += 15*(1//(places[place_id]['rating']))
+            rank+= 20*(places[place_id]['distance']/dist)
             places[place_id]['rank'] = rank
 
     for id in bad_ids:
         del places[id]
-    print(places)
+    #print(places)
     if len(places) == 0:
-        print("none")
+    #    print("none")
         return None
     r = random.randint(0,3)
-    if r > len(places):
-        print('in loop')
+    if r >= len(places):
+    #    print('in loop')
         r = 0
+    print(r)
     output = [(k,v) for k, v in sorted(places.items(), key=lambda item: item[1]['rank'])]
     return (output[r])
 
